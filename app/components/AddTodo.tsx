52d908a90df7cbc9d01720e066e3d087311800c5
Ajoy Kumar Das <ajoykumardas08@gmail.com>
2023-05-18 09:56:50 +0530

    structure

---
+++ app/components/AddTodo.tsx
@@ -0,0 +1,12 @@
+function AddTodo() {
+    return (
+        <div>
+            <input type="text" name="newTodo" id="" />
+            <button>
+                Add
+            </button>
+        </div>
+    );
+}
+
+export default AddTodo;
---
+++ app/components/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <header className="text-center text-3xl font-medium">
+            Taskmaster
+        </header>
+    );
+}
+
+export default Header;
